87d74f1ae14de023f40adfc0c27750f8da6ed8b6
shahkv95 <kushshah253@gmail.com>
2022-12-24 17:55:31 +0530

    Created LCG class

---
+++ lcg.py
@@ -0,0 +1,12 @@
+class LCG:
+    def __init__(self, modulus, multiplier, increment):
+        self.modulus = modulus
+        self.multiplier = multiplier
+        self.increment = increment
+
+    def generate(self, seed):
+        """
+        Generates a random integer using a linear congruential generator (LCG).
+        """
+        seed = (self.multiplier * seed + self.increment) % self.modulus
+        return seed
