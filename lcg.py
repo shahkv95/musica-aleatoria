8f98b1123210030d5686ac8bd8d5b83612e9900d
shahkv95 <kushshah253@gmail.com>
2022-12-24 19:45:15 +0530

    Fixed Zero Devision Exception, didn't raise ValueError intentionally

--- lcg.py
+++ lcg.py
@@ -1,6 +1,9 @@
 class LCG:
     def __init__(self, modulus, multiplier, increment):
-        self.modulus = modulus
+        if modulus == 0:
+            self.modulus = 1
+        else:
+            self.modulus = modulus
         self.multiplier = multiplier
         self.increment = increment
 
